2e47766374e23d33a1a979c17cfd227a908fd752
alexandretta <alexsalazar21@gmail.com>
2023-01-21 15:36:22 -0500

    Se agregan algunos estilos experimentales a los background

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import { useState } from "react";
 
 // Se crea una card reutilizable para contener las preguntas y opciones, recibe props para cada una en la vista cards.
 const Card = (props) => {
@@ -13,20 +14,31 @@ const Card = (props) => {
             id='option1'
             name='cantante'
             value='option1'
-            checked
-          />
+            checked />
           <label for='option1'>{props.option1}</label>
         </p>
         <p>
-          <input type='radio' id='option2' name='cantante' value='option2' />
+          <input 
+            type='radio' 
+            id='option2' 
+            name='cantante' 
+            value='option2' />
           <label for='option2'>{props.option2}</label>
         </p>
         <p>
-          <input type='radio' id='option3' name='cantante' value='option3' />
+          <input 
+            type='radio' 
+            id='option3' 
+            name='cantante' 
+            value='option3' />
           <label for='option3'>{props.option3}</label>
         </p>
         <p>
-          <input type='radio' id='option4' name='cantante' value='option4' />
+          <input 
+            type='radio' 
+            id='option4' 
+            name='cantante' 
+            value='option4' />
           <label for='option4'>{props.option4}</label>
         </p>
         <Button text={"Enviar"} />
